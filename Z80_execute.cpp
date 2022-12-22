1962c8949fc10c0329603f5a51ab9b6c78817d46
Andreas Taylor <Andy4495@outlook.com>
2022-12-21 21:35:40 -0600

    Add BIT, SET, RES instructions

--- Z80_execute.cpp
+++ Z80_execute.cpp
@@ -1147,13 +1147,162 @@ void Z80::execute_misc_opcode() {  // IR[0] = 0xED
 }
 
 void Z80::execute_bit_opcode() {  // IR[0] == 0xCB
+    unsigned char *r = nullptr;   // Temporary storage when decoding register field in opcode
 
     switch (IR[1]) {
-        case 0x00:  // RLC B
+        // RLC B  (0xCB00)
+        case 0x00:
             if (B & 0x80) setFlag(C_BIT); else clearFlag(C_BIT);
             B = B << 1;
             B = (B & 0xFE) | testFlag(C_BIT);
             update_flags(S_BIT|Z_BIT|H_BIT|PV_BIT|N_BIT, BIT, B, 0);
+            break;
+
+        // BIT b, r (0xCB40 - 0xCB7F)
+        // BIT b, (HL)  (0xX6, 0xXE)
+        case 0x40: case 0x41: case 0x42: case 0x43: case 0x44: case 0x45: case 0x46: case 0x47:
+        case 0x48: case 0x49: case 0x4a: case 0x4b: case 0x4c: case 0x4d: case 0x4e: case 0x4f:
+        case 0x50: case 0x51: case 0x52: case 0x53: case 0x54: case 0x55: case 0x56: case 0x57:
+        case 0x58: case 0x59: case 0x5a: case 0x5b: case 0x5c: case 0x5d: case 0x5e: case 0x5f:
+        case 0x60: case 0x61: case 0x62: case 0x63: case 0x64: case 0x65: case 0x66: case 0x67:
+        case 0x68: case 0x69: case 0x6a: case 0x6b: case 0x6c: case 0x6d: case 0x6e: case 0x6f:
+        case 0x70: case 0x71: case 0x72: case 0x73: case 0x74: case 0x75: case 0x76: case 0x77:
+        case 0x78: case 0x79: case 0x7a: case 0x7b: case 0x7c: case 0x7d: case 0x7e: case 0x7f:
+        // Opcode 0  1  b  b  b  r  r  r  
+        // Decode the register, r
+            switch (IR[1] & 0x07) {
+                case 0b000:
+                    r = &B;
+                    break;
+                case 0b001:
+                    r = &C;
+                    break;
+                case 0b010:
+                    r = &D;
+                    break;
+                case 0b011:
+                    r = &E;
+                    break;
+                case 0b100:
+                    r = &H;
+                    break;
+                case 0b101:
+                    r = &L;
+                    break;
+                case 0b110:
+                    r = &memory[(H<<8) + L];
+                    break;
+                case 0b111:
+                    r = &A;
+                   break;
+                default: 
+                    r = nullptr;
+                    cout << "Execution not defined: 0xcb" << hex << setw(2) << (unsigned int) IR[1] << endl;
+                    break;
+            }
+            // Decode the bit position, b
+            if (r != nullptr) {
+                if ((*r) >> (IR[1] >> ((IR[1] & 0x3f) >> 3)) == 0) setFlag(Z_BIT);
+                else clearFlag(Z_BIT);
+                setFlag(H_BIT);
+                clearFlag(N_BIT);
+            }
+            break;
+
+        // SET b, r (0xCBC0 - 0xCBFF)
+        // SET b, (HL)  (0xX6, 0xXE)
+        case 0xc0: case 0xc1: case 0xc2: case 0xc3: case 0xc4: case 0xc5: case 0xc6: case 0xc7:
+        case 0xc8: case 0xc9: case 0xca: case 0xcb: case 0xcc: case 0xcd: case 0xce: case 0xcf:
+        case 0xd0: case 0xd1: case 0xd2: case 0xd3: case 0xd4: case 0xd5: case 0xd6: case 0xd7:
+        case 0xd8: case 0xd9: case 0xda: case 0xdb: case 0xdc: case 0xdd: case 0xde: case 0xdf:
+        case 0xe0: case 0xe1: case 0xe2: case 0xe3: case 0xe4: case 0xe5: case 0xe6: case 0xe7:
+        case 0xe8: case 0xe9: case 0xea: case 0xeb: case 0xec: case 0xed: case 0xee: case 0xef:
+        case 0xf0: case 0xf1: case 0xf2: case 0xf3: case 0xf4: case 0xf5: case 0xf6: case 0xf7:
+        case 0xf8: case 0xf9: case 0xfa: case 0xfb: case 0xfc: case 0xfd: case 0xfe: case 0xff:
+        // Opcode 1  1  b  b  b  r  r  r  
+        // Decode the register, r
+            switch (IR[1] & 0x07) {
+                case 0b000:
+                    r = &B;
+                    break;
+                case 0b001:
+                    r = &C;
+                    break;
+                case 0b010:
+                    r = &D;
+                    break;
+                case 0b011:
+                    r = &E;
+                    break;
+                case 0b100:
+                    r = &H;
+                    break;
+                case 0b101:
+                    r = &L;
+                    break;
+                case 0b110:
+                    r = &memory[(H<<8) + L];
+                    break;
+                case 0b111:
+                    r = &A;
+                   break;
+                default: 
+                    r = nullptr;
+                    cout << "Execution not defined: 0xcb" << hex << setw(2) << (unsigned int) IR[1] << endl;
+                    break;
+            }
+            // Decode the bit position, b
+            if (r != nullptr) {
+                *r |= (1 << ((IR[1] & 0x3f) >> 3));
+            }
+            break;
+
+        // RES b, r (0xCB80 - 0xCBbF)
+        // RES b, (HL)  (0xX6, 0xXE)
+        case 0x80: case 0x81: case 0x82: case 0x83: case 0x84: case 0x85: case 0x86: case 0x87:
+        case 0x88: case 0x89: case 0x8a: case 0x8b: case 0x8c: case 0x8d: case 0x8e: case 0x8f:
+        case 0x90: case 0x91: case 0x92: case 0x93: case 0x94: case 0x95: case 0x96: case 0x97:
+        case 0x98: case 0x99: case 0x9a: case 0x9b: case 0x9c: case 0x9d: case 0x9e: case 0x9f:
+        case 0xa0: case 0xa1: case 0xa2: case 0xa3: case 0xa4: case 0xa5: case 0xa6: case 0xa7:
+        case 0xa8: case 0xa9: case 0xaa: case 0xab: case 0xac: case 0xad: case 0xae: case 0xaf:
+        case 0xb0: case 0xb1: case 0xb2: case 0xb3: case 0xb4: case 0xb5: case 0xb6: case 0xb7:
+        case 0xb8: case 0xb9: case 0xba: case 0xbb: case 0xbc: case 0xbd: case 0xbe: case 0xbf:
+        // Opcode 1  0  b  b  b  r  r  r  
+        // Decode the register, r
+            switch (IR[1] & 0x07) {
+                case 0b000:
+                    r = &B;
+                    break;
+                case 0b001:
+                    r = &C;
+                    break;
+                case 0b010:
+                    r = &D;
+                    break;
+                case 0b011:
+                    r = &E;
+                    break;
+                case 0b100:
+                    r = &H;
+                    break;
+                case 0b101:
+                    r = &L;
+                    break;
+                case 0b110:
+                    r = &memory[(H<<8) + L];
+                    break;
+                case 0b111:
+                    r = &A;
+                   break;
+                default: 
+                    r = nullptr;
+                    cout << "Execution not defined: 0xcb" << hex << setw(2) << (unsigned int) IR[1] << endl;
+                    break;
+            }
+            // Decode the bit position, b
+            if (r != nullptr) {
+                *r &= ~(1 << ((IR[1] & 0x3f) >> 3));
+            }
             break;
 
         default: 
